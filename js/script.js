0a7668ca52986207f1c2e56ebaec28700f93bc7a
leviVieiraJr <levi.junior@sou.unifeob.edu.br>
2025-04-26 15:30:26 -0300

    mensagem

--- js/script.js
+++ js/script.js
@@ -11,4 +11,31 @@ function toggleMenu() {
     const submenu = event.target.nextElementSibling;
     submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
   }
+
+  // Função para pegar parâmetros da URL
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+// Preencher os dados
+document.addEventListener("DOMContentLoaded", function() {
+  const nomeProduto = getQueryParam('nome');
+  const precoAntigo = getQueryParam('precoAntigo');
+  const precoNovo = getQueryParam('precoNovo');
+  const imagem = getQueryParam('imagem');
+
+  if (nomeProduto) {
+    document.querySelector('.categoria-produto').textContent = nomeProduto;
+  }
+  if (precoAntigo) {
+    document.querySelector('.preco-antigo').textContent = precoAntigo;
+  }
+  if (precoNovo) {
+    document.querySelector('.preco-novo').textContent = precoNovo;
+  }
+  if (imagem) {
+    document.querySelector('.imagem-produto img').src = './assets/' + imagem;
+  }
+});
   
